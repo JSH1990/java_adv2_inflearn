ddd4801937331a8196aebe8406a6a53971b5cf1f
jshblack <118979741+JSH1990@users.noreply.github.com>
2024-11-27 22:07:19 +0900

    http2

--- src/was/v1/HttpRequestHandlerV2.java
+++ src/was/v1/HttpRequestHandlerV2.java
@@ -1,44 +1,30 @@
 package was.v1;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-
-import static java.lang.Thread.sleep;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static util.MyLogger.log;
-
-public class HttpServerV1 {
-    private final int port;
-
-    public HttpServerV1(int port) {
-        this.port = port;
+public class HttpRequestHandlerV2 implements Runnable {
+    private final Socket socket;
+    public HttpRequestHandlerV2(Socket socket) {
+        this.socket = socket;
     }
-
-    public void start() throws IOException {
-        ServerSocket serverSocket = new ServerSocket(port);
-        log("서버 시작 port: " + port);
-        while (true) {
-            Socket socket = serverSocket.accept();
-            process(socket);
+    @Override
+    public void run() {
+        try {
+            process();
+        } catch (Exception e) {
+            log(e);
         }
     }
-
-    private void process(Socket socket) throws IOException {
+    private void process() throws IOException {
         try (socket;
              BufferedReader reader = new BufferedReader(new
                      InputStreamReader(socket.getInputStream(), UTF_8));
              PrintWriter writer = new PrintWriter(socket.getOutputStream(),
                      false, UTF_8)) {
-            //autoFlush를 true로 하면 println()으로 출력할때마다 자동으로 플러쉬된다.
-            //첫내용을 빠르게 전송할 수 있지만, 네트워크 전송이 자주 발생한다.
-            //이 값을 false로 설정하면 flush()를 직접 호출해주어야 데이터를 전송한다.
-            //데이터를 모아서 전송하므로, 네트워크 전송 횟수를 효과적으로 줄일수 있다.
-            //한 패킷에 많은 양의 데이터를 담아서 전송할수있다.
             String requestString = requestToString(reader);
             if (requestString.contains("/favicon.ico")) {
                 log("favicon 요청");
@@ -47,18 +33,11 @@ public class HttpServerV1 {
             log("HTTP 요청 정보 출력");
             System.out.println(requestString);
             log("HTTP 응답 생성중...");
-            sleep(5000); // 서버 처리 시간
+            sleep(5000);
             responseToClient(writer);
             log("HTTP 응답 전달 완료");
         }
     }
-
-    /*
-    Http 요청을 읽어서 String으로 변환한다.
-    Http 요청이 시작라인. 헤더까지 읽는다.
-    - line.isEmpty()이면 Http 메세지 헤더의 마지막으로 인식하고, 메세지 읽기를 종료한다.
-    - Http 메세지 헤더의 끝은 빈라인으로 구분할수 있다. 빈라인 이후에는 메세지 바디가 나온다.
-     */
     private String requestToString(BufferedReader reader) throws IOException {
         StringBuilder sb = new StringBuilder();
         String line;
@@ -70,13 +49,6 @@ public class HttpServerV1 {
         }
         return sb.toString();
     }
-
-    /*
-    Http 응답 메세지를 생성해서 클라이언트에 전달한다.
-    시작라인, 헤더, http 메세지 바디를 전달한다.
-    마지막에 writer.flush()를 호출해서 데이터를 전송한다.
-    http 규약에서 엔터는 \r\n 로 표현한다.
-     */
     private void responseToClient(PrintWriter writer) {
 // 웹 브라우저에 전달하는 내용
         String body = "<h1>Hello World</h1>";
@@ -86,14 +58,12 @@ public class HttpServerV1 {
         sb.append("Content-Type: text/html\r\n");
         sb.append("Content-Length: ").append(length).append("\r\n");
         sb.append("\r\n"); // header, body 구분 라인
-        sb.append(body);
+        sb.append(body); // header, body 구분 라인
         log("HTTP 응답 정보 출력");
         System.out.println(sb);
         writer.println(sb);
         writer.flush();
     }
-
-    //서버에서 요청이 빠르기때문에 일부러 늦추기 위해 사용
     private void sleep(int millis) {
         try {
             Thread.sleep(millis);
---
+++ src/was/v1/HttpServerV2.java
@@ -0,0 +1,30 @@
+package was.v1;
+import was.v1.HttpRequestHandlerV2;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import static util.MyLogger.log;
+
+/*
+스레드를 생성하고 관리해준다.
+ */
+public class HttpServerV2 {
+    //스레드풀을 사용한다. 여기서는 newFixedThreadPool(10)을 사용해서 최대 동시에 10개의 스레드를 사용할수있게했다.
+    //결과적으로 10개의 요청을 동시에 처리할수있다.
+    private final ExecutorService es = Executors.newFixedThreadPool(10);
+    private final int port;
+    public HttpServerV2(int port) {
+        this.port = port;
+    }
+    public void start() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+        log("서버 시작 port: " + port);
+        while (true) {
+            Socket socket = serverSocket.accept();
+            es.submit(new HttpRequestHandlerV2(socket));
+        }
+    }
+}
