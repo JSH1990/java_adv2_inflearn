5e1b08c998dd7c7176d198166b7481bdc4b38cf5
jshblack <118979741+JSH1990@users.noreply.github.com>
2024-11-28 22:09:01 +0900

    http3

--- src/was/v1/ServerMainV3.java
+++ src/was/v1/ServerMainV3.java
@@ -1,12 +1,11 @@
 package was.v1;
 
-import was.v1.HttpServerV1;
-
 import java.io.IOException;
-public class ServerMainV1 {
+
+public class ServerMainV3 {
     private static final int PORT = 12345;
     public static void main(String[] args) throws IOException {
-        HttpServerV2 server = new HttpServerV2(PORT);
+        HttpServerV3 server = new HttpServerV3(PORT);
         server.start();
     }
 }
